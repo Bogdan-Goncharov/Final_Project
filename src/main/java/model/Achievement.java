953d22698e55f6c10998874cdca442dbd96f3dbb
Bogdan-Goncharov <gonharov-bogdan@internet.ru>
2025-05-18 19:23:31 +0300

    Add all model

---
+++ src/main/java/model/Achievement.java
@@ -0,0 +1,25 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "achievement")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Achievement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private int points;
+}
---
+++ src/main/java/model/PlayerStats.java
@@ -0,0 +1,23 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "player_stats")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerStats {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int totalPoints;
+    private int totalAchievements;
+    private int ranking;
+}
